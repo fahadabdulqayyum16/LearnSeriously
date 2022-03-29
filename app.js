6074c7751441dc6e4ddc1f1e14da4161b39a1f75
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2022-03-29 10:52:14 +0500

    done

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const PORT = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    try {
+        return res.send({ message: `Hello NodeJs server is Running on port ${PORT}!`, success: true })
+    } catch (e) {
+        console.log('e', e);
+        return res.send({ message: e?.message, success: false })
+    }
+})
 
 // app.use('/', (req, res) => {
 //     console.log();
